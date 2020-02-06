1b36717b48df0399e0a7fe2c41bc1a8c7d72b224
Sumanth <sumanth.bharadwaj@profinch.com>
2020-02-06 10:42:09 +0530

    Initial commit

---
+++ abc.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class userinput {
+    public static void main(String[] args){
+        Scanner in =new Scanner(System.in);
+
+
+                System.out.println("enter an integer");
+                int a=in.nextInt();
+                System.out.println("nfl=g"+ a);
+    }
+}
